773a4a430d4e7069b9eb0f001c39a31b8bf6e3c0
Jesse Bremer <jsr.bremer@gmail.com>
2022-05-19 13:05:36 -0400

    Created User interface, need to rework script.js. For now, app is not fuctional.

--- script.js
+++ script.js
@@ -5,42 +5,24 @@ let cpuScore = 0;
 
 // Call a Function to Start Game
 
-// function game() {
-//     for (let i = 1; i < 6; i++) {
-//         console.log(`Round: ${i}.`);
-//         playRound(userInput(), computerPlay());
-//         console.log(`Score: User:${userScore} - CPU:${cpuScore}`)
-//         console.log('');
-//         if (userScore > cpuScore && i == 5) {
-//             console.log(`You won ${userScore} - ${cpuScore}.`)
-//         } else if (userScore < cpuScore && i == 5) {
-//             console.log(`You lost ${userScore} - ${cpuScore}.`)
-//         } else if (userScore == cpuScore && i == 5) {
-//             console.log(`You tied ${userScore} - ${cpuScore}.`)
-//         } else if (i == 5) {
-//             console.log('Something went wrong.')
-//         }
-//     }
+function game() {
+    for (let i = 1; i < 6; i++) {
+        console.log(`Round: ${i}.`);
+        playRound(userInput(), computerPlay());
+        console.log(`Score: User:${userScore} - CPU:${cpuScore}`)
+        console.log('');
+        if (userScore > cpuScore && i == 5) {
+            console.log(`You won ${userScore} - ${cpuScore}.`)
+        } else if (userScore < cpuScore && i == 5) {
+            console.log(`You lost ${userScore} - ${cpuScore}.`)
+        } else if (userScore == cpuScore && i == 5) {
+            console.log(`You tied ${userScore} - ${cpuScore}.`)
+        } else if (i == 5) {
+            console.log('Something went wrong.')
+        }
+    }
 
-// }
-
-const rock = document.getElementById("Rock").addEventListener("click", () => {
-    console.log('rock');
-    return Rock;
-    playRound();
-});
-
-const paper = document.getElementById("Paper").addEventListener("click", () => {
-    console.log('paper');
-    return Paper;
-    playRound();
-});
-
-const scissors = document.getElementById("Scissors").addEventListener("click", () => {
-    console.log('scissors');
-    return Scissors;
-    playRound();
-});
+}
 
 // function userInput() {
 //     let userAnswer = prompt('Rock, Paper, or Scissors?');
@@ -48,6 +30,9 @@ const scissors = document.getElementById("Scissors").addEventListener("click", (
 //     console.log(`User: ${sensitiveAnswer}`);
 //     return sensitiveAnswer;
 // }
+
+
+
 
 // Computer Randomizes own Answer
 function computerPlay() {
@@ -57,30 +42,32 @@ function computerPlay() {
   return computerSelection;
 }
 
-// User and Computer compare Answer
 
-function playRound(, computerSelection) {
-  if (rock == "Rock" && computerSelection == "Paper") {
+// Change function arguments to take event buttons to pass through comparison checks.
+
+function playRound(userChoice, computerSelection) {
+  if (userChoice == "Rock" && computerSelection == "Paper") {
     console.log("You lose, paper beats rock!");
     cpuScore++;
-  } else if (paper == "Paper" && computerSelection == "Scissors") {
+  } else if (userChoice == "Paper" && computerSelection == "Scissors") {
     console.log("You lose, scissors beat paper!");
     cpuScore++;
-  } else if (scissors == "Scissors" && computerSelection == "Rock") {
+  } else if (userChoice == "Scissors" && computerSelection == "Rock") {
     console.log("You lose, rock beats scissors!");
     cpuScore++;
-  } else if (rock == "Rock" && computerSelection == "Scissors") {
+  } else if (userChoice == "Rock" && computerSelection == "Scissors") {
     console.log("You win, rock beats scissors!");
     userScore++;
-  } else if (paper == "Paper" && computerSelection == "Rock") {
+  } else if (userChoice == "Paper" && computerSelection == "Rock") {
     console.log("You win, paper beats rock!");
     userScore++;
-  } else if (scissors == "Scissors" && computerSelection == "Paper") {
+  } else if (userChoice == "Scissors" && computerSelection == "Paper") {
     console.log("You win, scissors beat paper!");
     userScore++;
-  } else if (rock == computerSelection || paper == computerSelection || scissors == computerSelection) {
+  } else if (userChoice == computerSelection) {
     console.log("It's a tie!");
   } else {
     console.log("Check again.");
   }
 }
+
